9eae9197fcba97775043ebd7693fec04d4a6e530
Renatoblp16 <55216929+Renatoblp16@users.noreply.github.com>
2019-09-29 18:21:12 -0300

    Add files via upload

---
+++ pratica02exercicio07.js
@@ -0,0 +1,5 @@
+alert ("Você esta recebendo uma notificação de confirmação para a festa de aniversário do Enzo.");
+
+var name= prompt ("Confirme sua presença informando seu nome: ");
+
+document.write ("Obrigado " + name + "!");
